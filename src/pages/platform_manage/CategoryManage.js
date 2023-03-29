256cfd9f910788bf6cde08135151209be4ca6b81
kushbrown <duddnmail@gmail.com>
2023-03-29 14:51:22 +0900

    feat: 광고주 카테고리 관리 퍼블리싱

--- src/pages/platform_manage/CategoryManage.js
+++ src/pages/platform_manage/CategoryManage.js
@@ -7,11 +7,124 @@ import {
   RowSpan, SearchButton,
   SearchInput
 } from "../../assets/GlobalStyles";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {CategoryContainer} from "./styles";
 import styled from "styled-components";
+import {atom, useAtom} from "jotai/index";
+import {atomWithReset, useResetAtom} from "jotai/utils";
+const categoryList = [
+  {
+    code: 'IAB1',
+    label: '예술 및 엔터테인먼트'
+  },
+  {
+    code: 'IAB2',
+    label: '자동차'
+  },
+  {
+    code: 'IAB3',
+    label: '사업'
+  },
+  {
+    code: 'IAB4',
+    label: '채용'
+  },
+  {
+    code: 'IAB5',
+    label: '교육'
+  },
+  {
+    code: 'IAB6',
+    label: '가족 및 육아'
+  },
+]
+
+const subCategoryList = [
+  {
+    code: 'IAB1',
+    category:['도서 및 문학','연예인 팬/가십','미술','유머','영화','음악','텔레비전']
+  },
+  {
+    code: 'IAB2',
+    category: ['자동차 부품','자동차 수리','자동차 매매','자동차 문화','인증 중고','컨버터블','쿠페','크로스 오버','디젤','전기자동차']
+  },
+  {
+    code: 'IAB3',
+    category: ['광고','농업','생명공학/생물의학','비지니스 소프트웨어','건설','영억','정부','친환경 솔루션']
+  },
+  {
+    code: 'IAB4',
+    category: ['경력 걔획','칼리지','재정 지원','취업 박람회','구직','이력서 적성/조언','간호','장학금']
+  },
+  {
+    code: 'IAB5',
+    category: ['미술사','대학 행정','대학 생활','원격 학습','제 2언어로서의 영어', '언어 학습' ,'대학원','홈스쿨링']
+  },
+  {
+    code: 'IAB6',
+    category: ['재택','영유아','데이케어/프리스쿨','제품군 인터넷','육아','키즈','청소년 양육']
+  },
+]
+
+const selectCategoryAtom = atom('')
+
+const createCategoryAtom = atomWithReset({
+  category: '',
+  subCategory: ''
+})
+
+async function createApi(data) {
+  try{
+
+    return 200
+  }catch (e) {
+    console.log(e)
+  }
+}
 
 export function CategoryManage() {
+  const [selectCategory, setSelectCategory] = useAtom(selectCategoryAtom)
+  const [createCategory, setCreateCategory] = useAtom(createCategoryAtom)
+  const resetCategory = useResetAtom(createCategoryAtom)
+  const [category, setCategory] = useState(false)
+  const [searchKeyword, setSearchKeyword] = useState('')
+
+  useEffect(() => {
+    resetCategory()
+  }, [category]);
+
+  const handleSelectCategory = (code) => {
+    setSelectCategory(code)
+  }
+
+  const handleChangeCategory = (inputEvent) => {
+    setCreateCategory({
+      ...createCategory,
+      category: inputEvent.target.value
+    })
+  }
+
+  const handleChangeSubCategory = (inputEvent) => {
+    setCreateCategory({
+      ...createCategory,
+      subCategory: inputEvent.target.value
+    })
+  }
+
+  const handleSearchCategory = (e) => {
+    console.log(e.target.value)
+    setSearchKeyword(e.target.value)
+  }
+
+  const handleCreateCategory = async () => {
+    const fetchData = await createApi(createCategory.category)
+    console.log(fetchData)
+  }
+  const handleCreateSubCategory = async () => {
+    const fetchData = await createApi(createCategory.subCategory)
+    console.log(fetchData)
+  }
+
   return(
     <>
       <Board>
@@ -20,7 +133,7 @@ export function CategoryManage() {
           <RowSpan>
             <ColSpan3/>
             <ColSpan1>
-              <Input/>
+              <Input value={searchKeyword} onChange={handleSearchCategory}/>
               <SearchButton>검색</SearchButton>
             </ColSpan1>
           </RowSpan>
@@ -29,28 +142,49 @@ export function CategoryManage() {
           <MainCategory>
             <CategoryHeader>
               <div>카테고리</div>
-              <EnrollButton>추가</EnrollButton>
+              <EnrollButton onClick={() => setCategory(!category)}>{category ? "취소" : "추가"}</EnrollButton>
             </CategoryHeader>
-            <CategoryEnroll>
-              <ColSpan3><Input/></ColSpan3>
-              <ColSpan1><DefaultButton>등록</DefaultButton></ColSpan1>
+            <CategoryEnroll style={category ? {height: 60} : {height:0}}>
+              <ColSpan3>
+                <Input value={createCategory.category || ""} onChange={handleChangeCategory}/>
+              </ColSpan3>
+              <ColSpan1><DefaultButton onClick={handleCreateCategory}>등록</DefaultButton></ColSpan1>
             </CategoryEnroll>
             <CategoryBody>
-              <CategoryItem>예술 및 엔터테인먼트</CategoryItem>
+              {categoryList && categoryList.map((item, key) => {
+                return (
+                  <CategoryItem
+                    key={key}
+                    onClick={() => handleSelectCategory(item.code)}
+                    style={item.code === selectCategory? {color:"#ffbb00"} : null}>{item.label}</CategoryItem>
+                )
+              })}
             </CategoryBody>
           </MainCategory>
           <SubCategory>
             <CategoryHeader>
               <div>하위 카테고리</div>
-              <EnrollButton>등록</EnrollButton>
+              <EnrollButton onClick={()=>setCategory(!category)}>{category ? "취소" : "추가"}</EnrollButton>
             </CategoryHeader>
-            <CategoryEnroll>
-              <input/>
-              <ColSpan1><DefaultButton>등록</DefaultButton></ColSpan1>
+            <CategoryEnroll style={{justifyContent:'flex-end',height: category ? 60 : 0 }}>
+              <div style={{display:'flex',width: 400}}>
+                <ColSpan3>
+                  <Input value={createCategory.subCategory || ""} onChange={handleChangeSubCategory}/>
+                </ColSpan3>
+                <ColSpan1><DefaultButton onClick={handleCreateSubCategory}>등록</DefaultButton></ColSpan1>
+              </div>
             </CategoryEnroll>
-            <SubCategoryBody>
-              <SubCategoryItem>자동차 부품</SubCategoryItem>
-            </SubCategoryBody>
+              {selectCategory !== '' && subCategoryList.filter(item => item.code === selectCategory).map((item, key) => {
+                return(
+                  <SubCategoryBody key={key}>
+                    {item.category.map((sub, key) => {
+                      return (
+                        <SubCategoryItem key={key}>{sub}</SubCategoryItem>
+                      )
+                    })}
+                  </SubCategoryBody>
+                )
+              })}
           </SubCategory>
         </CategoryContainer>
       </Board>
@@ -69,8 +203,11 @@ const SubCategory = styled.div`
 
 const CategoryEnroll = styled.div`
   display: flex;
-  padding: 10px;
-  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  overflow: hidden;
+  padding: 0 10px 0 0;
+  height: 0;
+  transition-duration: 0.5s;
 `
 
 const CategoryHeader = styled.div`
@@ -79,30 +216,38 @@ const CategoryHeader = styled.div`
   padding: 20px;
   text-align: center;
   background-color: #f8f8f8;
+  border-bottom: 1px solid #ddd;
 `
 
 const CategoryBody = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   width: 100%;
+  & div:last-child {
+    border-bottom: 0;
+  }
 `
 
 const CategoryItem = styled.div`
   padding: 20px;
   width: 100%;
-  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  cursor: pointer;
 `
 
 const SubCategoryBody = styled.div`
   display: flex;
   flex-wrap: wrap;
   width: 100%;
+  & div:nth-child(4n) {
+    border-right: 0;
+  }
 `
 
 const SubCategoryItem = styled.div`
   padding: 20px;
   width: 25%;
-  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
   border-right: 1px solid #ddd;
 `
 
