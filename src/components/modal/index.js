c6bdb2bf43d71fd8d306b27a1edc6b32f9665d8f
chocto84 <chocto@findinglab.co.kr>
2023-03-30 16:17:40 +0900

    2ad8d50564154c5ca9ac2d71c69e40f6
    아이디 찾기 비번 찾기 api 연동

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -14,7 +14,7 @@ export function ComponentModalFindId(){
       isShow: false,
       modalComponent: null
     })
-    navigate('/login')
+    navigate('/')
   }
   return (
     <div>
@@ -22,9 +22,9 @@ export function ComponentModalFindId(){
       <ModalBody>
         <FindIdResult>아이디 찾기 결과 <span>{findIdResult !== undefined && findIdResult.length}개</span>의 아이디가 존재합니다.</FindIdResult>
         <ModalBodyInner>
-          {findIdResult.length !== 0 && findIdResult.map((item) => {
+          {findIdResult.length !== 0 && findIdResult.map((item,index) => {
             return (
-              <p>{item}</p>
+              <p key={index}>{item}</p>
             )
           })}
         </ModalBodyInner>
@@ -52,7 +52,7 @@ export function ComponentModalFindPassword(props) {
       isShow: false,
       modalComponent: null
     })
-    navigate('/login')
+    navigate('/')
   }
 
   return(
