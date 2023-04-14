306e0be8ae4c5a6349bb1dc9ddf2086f2b021799
kspring <kspring@pounding.kr>
2023-04-14 15:18:37 +0900

    4e680554afc04eae9f8b11673210d853 feat : 대시보드 광고주 현황 캠페인 리스트 프론트 작업중

--- src/pages/dash_board/DashBoardIndex.js
+++ src/pages/dash_board/DashBoardIndex.js
@@ -57,6 +57,7 @@ import {
   retrieveAdvertiserStatus
 } from "../../services/dash_board/ManageCampaignAxios";
 import {decimalFormat, moneyToFixedFormat, numberToFixedFormat} from "../../common/StringUtils";
+import {DashBoardCondition} from "../../components/Platform/Condition";
 
 /** 플래폼 현황 차트 **/
 function PlatformResponsiveBar(props) {
@@ -192,18 +193,13 @@ function PlatformResponsiveBar(props) {
 /** 대시보드 **/
 function DashBoardIndex() {
   const [tokenUserInfo] = useAtom(tokenResultAtom)
+  const [searchCondition, setSearchCondition] = useState(searchConditionAtom)
   const [totalInfo, setTotalInfo] = useState(dataTotalInfo)
   const [adverStatusData, setAdverStatusData] = useAtom(adverStatusAtom)
-  const [searchCondition, setSearchCondition] = useState(searchConditionAtom)
   const [platformStatusData, setPlatformStatusData] = useAtom(platformStatusAtom)
   const [lineData, setLineData] = useAtom(lineDataAtom)
   const [cloneLineData, setCloneLineData] = useAtom(cloneLineDataAtom)
-
-  const [dateRange, setDateRange] = useState([new Date(getThisMonth().startDay), new Date(getToDay())]);
-  const [startDate, endDate] = dateRange;
-
   const [platformChartTotal, setPlatformChartTotal] = useState(platformTotalCont)
-  const [isCheckedAll, setIsCheckedAll] = useState(true)
   const [dataType, setDataType] = useState('cpc')
   const [dataType2, setDataType2] = useState('costAmount')
   const [onOff, setOnOff] = useState(onOffStatus)
@@ -266,13 +262,7 @@ function DashBoardIndex() {
     }
   },[onOff])
 
-  useEffect(() => {
-    if (searchCondition.agentTypes.length === 4) {
-      setIsCheckedAll(true)
-    } else {
-      setIsCheckedAll(false)
-    }
-  }, [searchCondition.agentTypes]);
+
 
   const handlePlatformData = (response) => {
     let clickCount, exposureCount, totalConversionCount, userCount, totalExposureCount, totalClickCount, costAmount,
@@ -340,107 +330,7 @@ function DashBoardIndex() {
       setPlatformStatusData([])
     }
   }
-  /**
-   * 날짜 레인지 선택
-   * @param rangeType
-   */
-  const handleRangeDate = (rangeType) => {
-    if (rangeType === 'thisMonth') {
-      setSearchCondition({
-        ...searchCondition,
-        searchStartDate: getThisMonth().startDay,
-        searchEndDate: getThisMonth().endDay
-      })
-      setDateRange([new Date(getThisMonth().startDay), new Date(getThisMonth().endDay)])
-    } else if (rangeType === 'lastMonth') {
-      setSearchCondition({
-        ...searchCondition,
-        searchStartDate: getLastMonth().startDay,
-        searchEndDate: getLastMonth().endDay
-      })
-      setDateRange([new Date(getLastMonth().startDay), new Date(getLastMonth().endDay)])
-    } else if (rangeType === 'today') {
-      setSearchCondition({
-        ...searchCondition,
-        searchStartDate: getToDay(),
-        searchEndDate: getToDay()
-      })
-      setDateRange([new Date(), new Date()])
-    } else if (rangeType === 'lastDay') {
-      setSearchCondition({
-        ...searchCondition,
-        searchStartDate: getLastDay(),
-        searchEndDate: getLastDay()
-      })
-      setDateRange([new Date(getLastDay()), new Date(getLastDay())])
-    } else if (rangeType === 'lastWeekDay') {
-      setSearchCondition({
-        ...searchCondition,
-        searchStartDate: getLastWeekDay().startDay,
-        searchEndDate: getLastWeekDay().endDay
-      })
-      setDateRange([new Date(getLastWeekDay().startDay), new Date(getLastWeekDay().endDay)])
-    } else if (rangeType === 'lastThirtyDay') {
-      setSearchCondition({
-        ...searchCondition,
-        searchStartDate: getLastThirtyDay().startDay,
-        searchEndDate: getLastThirtyDay().endDay
-      })
-      setDateRange([new Date(getLastThirtyDay().startDay), new Date(getLastThirtyDay().endDay)])
-    }
-  }
-  /**
-   * 광고 상품 선택
-   * @param productType
-   */
-  const handleProductType = (selectProductType) => {
-    setSearchCondition({
-      ...searchCondition,
-      productType: selectProductType.value
-    })
-  }
-  const handleChangeCheckAll = (event) => {
-    if (event.target.checked) {
-      setSearchCondition({
-        ...searchCondition,
-        agentTypes: ['WEB', 'WEB_APP', 'MOBILE_WEB', 'MOBILE_NATIVE_APP']
-      })
-    } else {
-      setSearchCondition({
-        ...searchCondition,
-        agentTypes: []
-      })
-    }
-    setIsCheckedAll(event.target.checked)
-  }
-  /**
-   * 에이전트 타입 체크
-   * @param event
-   */
-  const handleChangeCheck = (event) => {
-    if (event.currentTarget.checked) {
-      setSearchCondition({
-        ...searchCondition,
-        agentTypes: searchCondition.agentTypes.concat(event.currentTarget.value)
-      })
-    } else {
-      setSearchCondition({
-        ...searchCondition,
-        agentTypes: searchCondition.agentTypes.filter(id => id !== event.currentTarget.value)
-      })
-    }
-  }
 
-  /**
-   * 검색어
-   * @param keyword
-   */
-  const handleSearchValue = (event) => {
-    setSearchCondition({
-      ...searchCondition,
-      keyword: event.target.value
-    })
-  }
 
   /**
    * 검색 버튼
@@ -483,112 +373,7 @@ function DashBoardIndex() {
   return (
     <>
       <DashBoardCard>
-        <BoardSearchDetail>
-          <RowSpan style={{marginTop: 0, justifyContent: 'flex-start'}}>
-            <ColSpan0 style={{marginRight: 20}}>
-              <ColTitle style={{paddingLeft: 0}}>광고 상품</ColTitle>
-              <Select components={{IndicatorSeparator: () => null}}
-                      options={productType}
-                      value={productType.find(value => value.value === searchCondition.productType)}
-                      onChange={handleProductType}
-                      styles={{
-                        input: (baseStyles, state) => (
-                          {
-                            ...baseStyles,
-                            width: "100px",
-                          })
-                      }}
-              />
-            </ColSpan0>
-            <ColSpan3>
-              <ColTitle style={{paddingLeft: 0}}>에이전트</ColTitle>
-              <RelativeDiv>
-                <AgentType>
-                  <Checkbox label={'전체'}
-                            type={'c'}
-                            id={'all'}
-                            value={'All'}
-                            isChecked={isCheckedAll}
-                            onChange={handleChangeCheckAll}
-                  />
-                  <Checkbox label={'PC 웹'}
-                            type={'c'}
-                            id={'WEB'}
-                            value={'WEB'}
-                            isChecked={searchCondition.agentTypes.includes('WEB') ? true : false}
-                            onChange={handleChangeCheck}/>
-                  <Checkbox label={'PC 어플리케이션'}
-                            type={'c'}
-                            id={'WEB_APP'}
-                            value={'WEB_APP'}
-                            isChecked={searchCondition.agentTypes.includes('WEB_APP') ? true : false}
-                            onChange={handleChangeCheck}/>
-                  <Checkbox label={'모바일 웹'}
-                            type={'c'}
-                            id={'MOBILE_WEB'}
-                            value={'MOBILE_WEB'}
-                            isChecked={searchCondition.agentTypes.includes('MOBILE_WEB') ? true : false}
-                            onChange={handleChangeCheck}/>
-                  <Checkbox label={'모바일 어플리케이션'}
-                            type={'c'}
-                            id={'MOBILE_NATIVE_APP'}
-                            value={'MOBILE_NATIVE_APP'}
-                            isChecked={searchCondition.agentTypes.includes('MOBILE_NATIVE_APP') ? true : false}
-                            onChange={handleChangeCheck}/>
-                </AgentType>
-              </RelativeDiv>
-            </ColSpan3>
-          </RowSpan>
-          <RowSpan style={{justifyContent: 'flex-start', marginTop: 20}}>
-            <ColSpan0>
-              <ColTitle style={{paddingLeft: 0}}>기간</ColTitle>
-              <div>
-                <DateContainer>
-                  <CalendarBox>
-                    <CalendarIcon/>
-                  </CalendarBox>
-                  <CustomDatePicker
-                    selectsRange={true}
-                    startDate={startDate}
-                    endDate={endDate}
-                    minDate={new Date(getLastThirtyDay().startDay)}
-                    maxDate={new Date()}
-                    onChange={(date) => setDateRange(date)}
-                    dateFormat="yyyy-MM-dd"
-                    locale={ko}
-                    isClearable={false}
-                  />
-                </DateContainer>
-              </div>
-              <div>
-                <RangePicker>
-                  <div onClick={() => handleRangeDate('thisMonth')}>이번달</div>
-                  <HorizontalRule style={{margin: "0 10px"}}/>
-                  <div onClick={() => handleRangeDate('lastMonth')}>지난달</div>
-                  <HorizontalRule style={{margin: "0 10px"}}/>
-                  <div onClick={() => handleRangeDate('today')}>오늘</div>
-                  <HorizontalRule style={{margin: "0 10px"}}/>
-                  <div onClick={() => handleRangeDate('lastDay')}>어제</div>
-                  <HorizontalRule style={{margin: "0 10px"}}/>
-                  <div onClick={() => handleRangeDate('lastWeekDay')}>지난7일</div>
-                  <HorizontalRule style={{margin: "0 10px"}}/>
-                  <div onClick={() => handleRangeDate('lastThirtyDay')}>지난30일</div>
-                </RangePicker>
-              </div>
-            </ColSpan0>
-            {tokenUserInfo.role !== 'NORMAL' &&
-              <ColSpan1 style={{marginLeft: 20}}>
-                <ColTitle style={{paddingLeft: 0}}>검색어</ColTitle>
-                <Input type={'text'}
-                       placeholder={'광고주명 및 아이디 검색'}
-                       value={searchCondition.keyword}
-                       onChange={handleSearchValue}
-                />
-                <SearchButton onClick={handleData}>검색</SearchButton>
-              </ColSpan1>
-            }
-          </RowSpan>
-        </BoardSearchDetail>
+        <DashBoardCondition role={tokenUserInfo.role} searchType={productType} searchCondition={searchCondition} setSearchCondition={setSearchCondition} handleData={handleData}/>
       </DashBoardCard>
       <DashBoardCard>
         <DashBoardHeader>플랫폼 현황</DashBoardHeader>
