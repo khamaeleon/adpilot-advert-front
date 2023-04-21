2c30990d1527b05fdda9a59b2f3820163e8267cc
byeongcheolryu <zomking@gmail.com>
2023-04-21 14:23:00 +0900

    d213101a840f4446b2b186ad2ee81f99
    환불 결제 방식 추가
    충전 금액 버튼식, 입력식, 초기화 기능 추가
    결제 금액 부가세 계산 값 추가
    결제 시 api 통신 관련 스크립트 목업 데이터 활용 테스트 버전 작성

--- src/components/payment/user/AdCharge.js
+++ src/components/payment/user/AdCharge.js
@@ -1,9 +1,10 @@
 import {useAtom} from "jotai";
+import axios from 'axios';
 import React, {useEffect, useState} from "react";
 import {ModalBody, ModalFooter, ModalHeader} from "../../modal/Modal";
 import styled from "styled-components";
 import {modalController} from "../../../store";
-import { ColSpan2, ColSpan3, ColSpan4, DefaultButton, RowSpan, RelativeDiv,} from "../../../assets/GlobalStyles";
+import { ColSpan2, ColSpan3, ColSpan4, DefaultButton, RowSpan, RelativeDiv, ValidationScript, SubmitButton} from "../../../assets/GlobalStyles";
 import {SmallButton} from "../../../pages/campaign/styles/common";
 import {decimalFormat, removeStr} from "../../../common/StringUtils";
 import {useForm} from "react-hook-form";
@@ -17,12 +18,13 @@ export function AdChargeButton(props) {
             width: 650,
             modalComponent: () => {
                 return (
-                    <AdChargeModal onSave={onSave}
-                                   modalInfo={modalInfo}
-                                   onSubmit={onSubmit}
-                                   title={title}
-                                   requestAmountValue={requestAmountValue}
-                                   setRequestAmountValue={setRequestAmountValue}
+                    <AdChargeModal
+                        onSave={onSave}
+                        modalInfo={modalInfo}
+                        onSubmit={onSubmit}
+                        title={title}
+                        requestAmountValue={requestAmountValue}
+                        setRequestAmountValue={setRequestAmountValue}
                     />
                 )
             }
@@ -32,36 +34,67 @@ export function AdChargeButton(props) {
 }
 
 function AdChargeModal (props) {
-    const {title, requestAmountValue, setRequestAmountValue} = props
-    const {register, handleSubmit, setValue, setError, formState:{errors} } = useForm()
-    const {chargeAmount, setChargeAmount} = useState(0) // 충전 금액
-    const [, setModal] = useAtom(modalController)
+    const [,setModal] = useAtom(modalController)
+    const {title, setRequestAmountValue} = props
+    const {register, handleSubmit, setError, formState:{errors} } = useForm()
+    const [chargeAmount, setChargeAmount] = useState(0) // 충전 금액
+    const [inputValue, setInputValue] = useState(0) // 인풋 클릭 여부
+    const [payMethod, setPayMethod] = useState('CARD'); // 결제 방식
 
-
-    const handleChange = (value) => {
-        setChargeAmount(value.target.value)
-        // let num = removeStr(value)
-        // let numberNum = Number(num)
-        // if(maxAmount < numberNum){
-        //     setError('requestAmountValue', {type: 'required', message:'정산 신청금이 잔여 정산금을 초과하였습니다.'})
-        // } else {
-        //     setRequestAmountValue(numberNum)
-        //     setValue('requestAmountValue', numberNum)
-        // }
-        //
-        // setRequestAmountValue(numberNum)
-        // setValue('requestAmountValue', numberNum)
-    }
+    const calcAmount = () => (chargeAmount / 10) + chargeAmount // 부가세 합한 충전 값
 
     const handleClickChargeButton = (plusValue) => {
-        setChargeAmount(chargeAmount => chargeAmount + plusValue);
+        if(inputValue === 1){
+            setChargeAmount(0)
+            setInputValue(0)
+            setChargeAmount(chargeAmount => chargeAmount + plusValue)
+        }else{
+            setChargeAmount(chargeAmount => chargeAmount + plusValue)
+        }
     }
-
-
-
+    const handleChange = (value) => {
+        let num = removeStr(value)
+        let numberNum = Number(num)
+        setChargeAmount(numberNum)
+    }
+    const handleReset = () => {
+        setChargeAmount(0)
+        setInputValue(1)
+    }
+    const payWithEasypay = async (paymentMethod, amount) => {
+        try {
+            const response = await axios.post('java로 보낼 주소', {
+                paymentMethod: paymentMethod,
+                amount: amount
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    const onSubmit = async () => {
+        if (chargeAmount <= 0) {
+            setError('chargeAmount', {type: 'required', message:'충전 금액을 입력해 주세요'})
+        } else {
+            // 거래타입, 충전금액 데이터 post 값으로 넘기기
+            setRequestAmountValue(requestAmountValue => requestAmountValue + calcAmount());
+            await payWithEasypay(payMethod, calcAmount)
+                .then(response => {
+                    console.log("성공 응답 처리",response);
+                    // 성공적인 응답 처리
+                    setModal({isShow: false});
+                })
+                .catch(error => {
+                    console.error("실패 응답 처리",error);
+                    // 실패한 응답 처리
+                    // 에러 메시지 등을 사용자에게 알려줄 수 있습니다.
+                });
+        }
+    }
+    const onError = () => console.log(errors)
 
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
             <ModalHeader title={title}/>
             <ModalBody>
                 <RowSpan>
@@ -74,16 +107,20 @@ function AdChargeModal (props) {
                                         <label>
                                             <input
                                                 type={'radio'}
-                                                name={'inventory'}
-                                                value={'auto'}
+                                                name={'paymentMethod'}
+                                                value="CARD"
+                                                checked={payMethod === 'CARD'}
+                                                onChange={(e) => setPayMethod(e.target.value)}
                                             />
                                             <span>카드 결제</span>
                                         </label>
                                         <label>
                                             <input
                                                 type={'radio'}
-                                                name={'inventory'}
-                                                value={'categories'}
+                                                name={'paymentMethod'}
+                                                value="BANK"
+                                                checked={payMethod === 'BANK'}
+                                                onChange={(e) => setPayMethod(e.target.value)}
                                             />
                                             <span>계좌이체</span>
                                         </label>
@@ -93,12 +130,22 @@ function AdChargeModal (props) {
                             <RowSpan>
                                 <ColSpan2>충전 금액</ColSpan2>
                                 <ColSpan3>
-                                    <RelativeDiv className={'won color-black'}>
+                                    <RelativeDiv className={'color-black'}>
                                         <Input
                                             type={'text'}
-                                            // value={decimalFormat(requestAmountValue)}
-                                            // onChange={(e)=>handleChange(e.target.value)}
+                                            value={decimalFormat(chargeAmount + ' 원')}
+                                            onClick={()=>handleReset()}
+                                            maxLength="19"
+                                            {...register("chargeAmount", {
+                                                required: "충전 금액을 입력해 주세요.",
+                                                pattern: {
+                                                    message: "숫자만 입력 가능합니다.",
+                                                    value: "^[0-9,]+원?$",
+                                                },
+                                                onChange:(e)=>handleChange(e.target.value)
+                                            })}
                                         />
+                                        {errors.chargeAmount && <ValidationScript style={{left:"0", bottom:"-16px"}}>{errors.chargeAmount.message}</ValidationScript>}
                                     </RelativeDiv>
                                 </ColSpan3>
                                 <ColSpan3>
@@ -111,13 +158,17 @@ function AdChargeModal (props) {
                                 <div style={{display: "flex", flexDirection:'column'}}>
                                     <ColSpan2>결제 금액
                                         <span>
-                                            <small>충전 금액</small>
+                                            <small>충전 금액 </small>
                                             <small className={'won'}>{decimalFormat(chargeAmount)}</small> |
-                                            <small> 부가세</small><small className={'won'}>{decimalFormat(chargeAmount)}</small>
+                                            <small> 부가세 </small><small className={'won'}>{decimalFormat(chargeAmount / 10)}</small>
                                         </span>
                                     </ColSpan2>
-                                    <ColSpan4 style={{padding:" 15px 0 0 73px", color:"#000", fontWeight:"bold"}}>
-                                        총 결제 금액 <span className={'won'}>{decimalFormat(chargeAmount)}</span>
+                                    <ColSpan4 style={{padding:"15px 0 0 73px", color:"#000", fontWeight:"bold"}}>
+                                        총 결제 금액
+                                        {chargeAmount?
+                                            (<span className={'won'} style={{color:'#f5811f'}}>{decimalFormat(calcAmount())}</span>)
+                                            :(<span className={'won'}>0</span>)}
+
                                     </ColSpan4>
                                 </div>
                             </RowSpan>
@@ -130,7 +181,7 @@ function AdChargeModal (props) {
                 </RowSpan>
             </ModalBody>
             <ModalFooter>
-                <DefaultButton>결제</DefaultButton>
+                <SubmitButton type={"submit"}>결제</SubmitButton>
             </ModalFooter>
         </form>
     )
@@ -138,7 +189,7 @@ function AdChargeModal (props) {
 /**스타일 시트**/
 const Input = styled.input `
   width: 200px;
-  font-size: 20px;
+  font-size: 18px;
   font-weight: 600;
   border: 1px solid #ddd;
   border-radius: 5px;
