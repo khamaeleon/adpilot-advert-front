d1b06499bf1a5293ba0c0e76665cfb9b9ee4e21b
kushbrown <duddnmail@gmail.com>
2023-05-15 18:31:40 +0900

    3b9b8664f60040909da97440c288d71a
    fix: 상품수집관리 단위테스트

--- src/pages/platform_manage/ProductManage.js
+++ src/pages/platform_manage/ProductManage.js
@@ -68,7 +68,9 @@ function ProductManage() {
       }
     })
   }, [])
-
+  /**
+   * 상품 수집 검색
+   */
   const handleSearchResult = () => {
     retrieveProduct(searchCondition).then(response =>{
       if(response){
@@ -82,13 +84,19 @@ function ProductManage() {
       <Board>
         <BoardHeader>상품 수집 현황</BoardHeader>
         <BoardSearchDetail>
-          <PlatformCondition searchType={searchProductType} searchCondition={searchCondition} setSearchCondition={setSearchCondition} handleTableData={handleSearchResult}/>
+          <PlatformCondition
+            searchType={searchProductType}
+            searchCondition={searchCondition}
+            setSearchCondition={setSearchCondition}
+            handleTableData={handleSearchResult}
+          />
         </BoardSearchDetail>
         <BoardSearchResult>
-          <Table columns={productListColumn}
-                 totalCount={[productData.totalCount !== undefined ? productData.totalCount : 0,'상품수']}
-                 data={productData.rows !== undefined ? productData.rows : []}
-                 idProperty={'id'}
+          <Table
+            columns={productListColumn}
+            totalCount={[productData.totalCount !== undefined ? productData.totalCount : 0,'상품수']}
+            data={productData.rows !== undefined ? productData.rows : []}
+            idProperty={'id'}
           />
         </BoardSearchResult>
       </Board>
