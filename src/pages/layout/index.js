63ee6215256d604d13e9cddc4260af5e6a74f235
byeongcheolryu <zomking@gmail.com>
2023-05-19 16:03:25 +0900

    fix : layout/index.js 사용자 광고비 잔액 올류 수정

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -72,19 +72,19 @@ function Layout() {
     }
   }, [params.id])
   //[d] 광고비 잔액
-  useEffect(() => {
-    const pointData = async () => {
-      try {
-        const userId = tokenUserInfo.id;
-        const response = await retrieveUserPointRequest(userId);
-        setUserPoint(response.availablePoint)
-      } catch (error) {
-        console.error("실패 응답 처리", error);
-      }
-    };
-
-    pointData();
-  }, [tokenUserInfo]);
+  // useEffect(() => {
+  //   const pointData = async () => {
+  //     try {
+  //       const userId = tokenUserInfo.id;
+  //       const response = await retrieveUserPointRequest(userId);
+  //       setUserPoint(response.availablePoint)
+  //     } catch (error) {
+  //       console.error("실패 응답 처리", error);
+  //     }
+  //   };
+  //
+  //   pointData();
+  // }, [tokenUserInfo]);
 
 
 
