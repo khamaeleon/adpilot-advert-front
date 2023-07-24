14436c3de4be9e97e32f433f468335468edc8802
chocto84 <chocto@findinglab.co.kr>
2023-07-24 11:29:09 +0900

    refresh 수정

--- src/common/AdverAxios.js
+++ src/common/AdverAxios.js
@@ -73,7 +73,7 @@ adverAxios.interceptors.response.use(
                 accessToken: data.token.accessToken
               })
               onTokenRefreshed(data.token.accessToken);
-          } else if (responseCode.statusCode ===403) {
+          } else if (responseCode.statusCode === 401 || responseCode.statusCode === 403) {
               refreshSubscribers = [];
               isTokenRefreshing = false;
               // eslint-disable-next-line no-restricted-globals
